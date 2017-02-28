9f4c4b912cc8566a9cfeecfe297425023d257fce
Eatingtec <eatingtec@163.com>
2017-02-28 09:57:11 +0800

    ready to work

---
+++ campus-ecommerce/src/main/java/com/seu/dm/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.seu.dm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Greeting on 2017/2/28.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value={"/","/index"})
+    public String index(){
+        return "index";
+    }
+}
