b9a753e952d7a3cf58477379f64fcbebc23fdca6
yinguangluoren <964753691@qq.com>
2017-03-02 21:09:31 +0800

    add the xml method of databas

---
+++ campus-ecommerce/src/main/java/com/seu/dm/entities/User.java
@@ -0,0 +1,105 @@
+package com.seu.dm.entities;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private Date createTime;
+
+    private String sex;
+
+    private Date birthdate;
+
+    private String address;
+
+    private String mobile;
+
+    private String hobby;
+
+    private Integer studentNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby == null ? null : hobby.trim();
+    }
+
+    public Integer getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(Integer studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+}
---
+++ campus-ecommerce/src/main/java/com/seu/dm/mappers/AccountMapper.java
@@ -0,0 +1,17 @@
+package com.seu.dm.mappers;
+
+import com.seu.dm.entities.Account;
+
+public interface AccountMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Account record);
+
+    int insertSelective(Account record);
+
+    Account selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Account record);
+
+    int updateByPrimaryKey(Account record);
+}
