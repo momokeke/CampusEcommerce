08c6c7eca2ec3456e2ccce064457aa098f391797
momokeke <1036944095@qq.com>
2017-03-13 13:19:10 +0800

    uuuup

---
+++ campus-ecommerce/src/main/java/com/seu/dm/dto/CartProductDTO.java
@@ -0,0 +1,63 @@
+package com.seu.dm.dto;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by momomo on 2017/3/12.
+ */
+public class CartProductDTO {
+    private Integer id;
+    private String name;
+    private Integer num;
+    private Integer pictureId;
+    private BigDecimal price;
+    private String shopName;
+
+    public Integer getPictureId() {
+        return pictureId;
+    }
+
+    public void setPictureId(Integer pictureId) {
+        this.pictureId = pictureId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+}
