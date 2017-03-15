3c5c1549994a84608cdb94a4e9ee700b93145b21
Eatingtec <eatingtec@163.com>
2017-03-15 21:51:15 +0800

    修改买家卖家不符合权限后的跳转

--- campus-ecommerce/src/main/resources/static/js/login.js
+++ campus-ecommerce/src/main/resources/static/js/login.js
@@ -1,14 +1,13 @@
 /**
  * Created by momomo on 2017/3/11.
  */
-jQuery(document).ready(function($) {
-    $('.theme-logIn').click(function(){
-        $('.theme-popover-mask').fadeIn(100);
-        $('.theme-popover').slideDown(200);
-    })
-    $('.theme-popTit .close').click(function(){
-        $('.theme-popover-mask').fadeOut(100);
-        $('.theme-popover').slideUp(200);
-    })
 
+$('.theme-logIn').click(function(){
+    $('.theme-popover-mask').fadeIn(100);
+    $('.theme-popover').slideDown(200);
 })
+$('.theme-popTit .close').click(function(){
+    $('.theme-popover-mask').fadeOut(100);
+    $('.theme-popover').slideUp(200);
+})
+
